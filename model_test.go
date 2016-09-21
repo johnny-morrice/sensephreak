119b1733fc9b117d101f0907d2888da39e93c96b
John Morrice <john@functorama.com>
2016-09-21 08:31:20 +0100

    Model passes unit tests

--- model_test.go
+++ model_test.go
@@ -5,13 +5,84 @@ import (
 )
 
 func Test_activeports(t *testing.T) {
-        t.Fail()
+        tests := mktests()
+
+        expect := []int{80, 90}
+
+        actual := tests.activeports()
+
+        for i, acp := range actual {
+                exp := expect[i]
+
+                if acp != exp {
+                        t.Error("Expected %v but received %v", exp, acp)
+                }
+        }
+
+        // Repeat to test the cache.
+        for i, acp := range actual {
+                exp := expect[i]
+
+                if acp != exp {
+                        t.Error("Expected %v (in cache) but received %v", exp, acp)
+                }
+        }
 }
 
 func Test_pass(t *testing.T) {
-        t.Fail()
+        rset := mkresults()
+
+        rset.pass(90)
+
+        expect := []int{80}
+
+        actual := rset.failports()
+
+        for i, acp := range actual {
+                exp := expect[i]
+
+                if acp != exp {
+                        t.Error("Expected %v but received %v", exp, acp)
+                }
+        }
 }
 
 func Test_failports(t *testing.T) {
-        t.Fail()
+        rset := mkresults()
+
+        expect := []int{80, 90}
+
+        actual := rset.failports()
+
+        for i, acp := range actual {
+                exp := expect[i]
+
+                if acp != exp {
+                        t.Error("Expected %v but received %v", exp, acp)
+                }
+        }
+}
+
+func mkresults() *resultset {
+        rset := &resultset{}
+        rset.tests = mktests()
+
+        return rset
+}
+
+func mktests() *testset {
+        tests := &testset{}
+
+        case80 := &testcase{}
+        case80.port = 80
+
+        case90 := &testcase{}
+        case90.port = 90
+
+        tests.cases = []*testcase{
+                case80,
+                case90,
+        }
+
+        return tests
 }
